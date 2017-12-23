48597978b2381d88f316a7be4bd9d90b6cfb7f76
Venkata Samala <venkata_samala@outlook.com>
2017-12-23 13:45:29 -0600

    OnlyCommit

--- Tuts1/Program.cs
+++ Tuts1/Program.cs
@@ -10,6 +10,14 @@ namespace Tuts1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please Enter your Age: ");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"Your Name is {name}");
+            Console.WriteLine($"Your Age is: {age}");
+            Console.ReadLine();
         }
     }
 }
